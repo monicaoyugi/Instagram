a95291235ef05a0385e90ad5c3fa5ac99fd17e9e
monicaoyugi <monicaoyugi@gmail.com>
2020-03-09 12:46:20 +0300

    add virtual

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/moringaschool/Documents/Core/Week1/pingram/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
