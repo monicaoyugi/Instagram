0633b4a33c435ce8e7697615eb9399f8483ff2a3
monicaoyugi <monicaoyugi@gmail.com>
2020-03-10 18:49:37 +0300

    add logout path to project url

--- instagram/urls.py
+++ instagram/urls.py
@@ -15,11 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('gram.urls')),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('logout/', views.LogoutView.as_view(
+        template_name="logout.html"
+    ))
 ]
