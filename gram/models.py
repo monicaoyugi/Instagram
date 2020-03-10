e75324e58ae0ff5a3f0273f2299048a5aa44d4a5
monicaoyugi <monicaoyugi@gmail.com>
2020-03-10 11:02:39 +0300

    defines save an delete functions for each model

--- gram/models.py
+++ gram/models.py
@@ -1,26 +1,74 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
+from tinymce.models import HTMLField
 
 
 # Create your models here.
 
+
 class Profile(models.Model):
-    username = models.OneToOneField(User, on_delete=models.CASCADE)
-    profile_picture = models.ImageField(default='default.png' ,upload_to='prof_images')
-    bio = models.TextField(max_length=120)
+    profile_photo = models.ImageField(upload_to='images')
+    bio = HTMLField()
+    user = models.OneToOneField(User,on_delete=models.CASCADE,primary_key=True)
+
+    def __str__(self):
+        return self.profile_photo.url
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    @classmethod
+    def update_bio(cls,id,new_bio):
+        cls.objects.filter(pk = id).update(bio=new_bio)
+        new_bio_object = cls.objects.get(bio = new_bio)
+        new_bio = new_bio_object.bio
+        return new_bio
 
 
 class Image(models.Model):
-    posted_by = models.ForeignKey('auth.User', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='images')
-    caption = models.TextField(max_length=50)
-    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
-    likes = models.ManyToManyField(User, blank=True, related_name='likes')
-    posted_on = models.DateTimeField(default=timezone.now)
+    Image = models.ImageField(upload_to='images/', blank=True)
+    image_name = models.CharField(max_length=50)
+    image_caption = models.CharField(max_length=50)
+    likes = models.PositiveIntegerField(default=0)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()
+
+    @classmethod
+    def update_caption(cls,id,new_caption):
+        cls.objects.filter(pk = id ).update(image_caption = new_caption)
+        new_caption_object = cls.objects.get(image_caption=new_caption)
+        new_caption = new_caption_object.image_caption
+        return new_caption
+
+    @classmethod
+    def get_single_photo(cls,id):
+        image = cls.objects.get(pk=id)
+        return image
+
+    def __str__(self):
+        return self.image_name
 
 class Comments(models.Model):
-    image = models.ForeignKey(Image, on_delete=models.CASCADE)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    Comment = models.TextField(max_length=150)
-    posted_on = models.DateTimeField(default=timezone.now)
+    comment = models.CharField(max_length=100,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True)
+
+    @classmethod
+    def get_comments(cls,id):
+        comments = cls.objects.filter(image__id=id)
+        return comments
+
+    def save_comment(self):
+        self.save()
+
+    def __str__(self):
+        return self.comment
