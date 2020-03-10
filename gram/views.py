604cf5a15c421ae0c57fa4f8640e5b8a8585af76
monicaoyugi <monicaoyugi@gmail.com>
2020-03-10 10:59:37 +0300

    removed image vote

--- gram/views.py
+++ gram/views.py
@@ -1,25 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . forms import ImageProfileForm, ImageUploadForm, CommentForm
 from .models import *
 from django.contrib.auth.decorators import login_required
+from vote.managers import VotableManager
 
 
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def index(request):
-    images = Image.object.all()
-
+    images = Image.objects.all()
     return render(request, 'index.html', {"images":images})
 
+def image_upload(request):
+    current_user = request.user
+    if request.method == 'POST':
+        form = ImageUploadForm(request.POST,request.FILES)
+        if form.is_valid():
+            image = form.save(commit=False)
+            image.user = current_user
+            image.save()
+        return redirect('index')
+    else:
+        form = ImageUploadForm()
+        return render(request,'upload.html', {"form":form})
 
-def profile(request):
+def profile_info(request):
     current_user = request.user
     profile_info = Profile.objects.filter(user=current_user).first()
     posts =  request.user.image_set.all()
 
     return render(request, 'profile.html',{"images":posts,"profile":profile_info,"current_user":current_user})
-
 
 
 
@@ -37,23 +48,14 @@ def profile_edit(request):
         form = ImageProfileForm()
         return render(request,'edit.html',{"form":form})
 
-def image_upload(request):
-    current_user = request.user
-    if request.method == 'POST':
-        form = ImageUploadForm(request.POST,request.FILES)
-        if form.is_valid():
-            image = form.save(commit=False)
-            image.user = current_user
-            image.save()
-        return redirect('index')
-    else:
-        form = ImageUploadForm()
-        return render(request,'upload.html', {"form":form})
+
 
 def comments(request,id):
     comments = Comments.get_comments(id)
     number = len(comments)
     return render(request,'comments.html',{"comments":comments,"number":number})
+
+
 
 
 def add_comment(request,id):
@@ -89,7 +91,7 @@ def search_user(request):
         search_term = request.GET.get("search_user")
         searched_user = User.objects.filter(username__icontains=search_term)
         message = f"{search_term}"
-        return render(request, 'socioapp/search_results.html', {"message": message, "users": searched_user})
+        return render(request, 'search_results.html', {"message": message, "users": searched_user})
     else:
         message = "No results found "
     return render(request, 'search_results.html',{"message":message})
